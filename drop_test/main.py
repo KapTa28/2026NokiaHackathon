bc5110024b9bf19de2814c3caaf70b045678f62f
Kapeller Tamás Imre <diak_105.kapeller.tamas.imre@10.c.2025.com>
2026-04-18 13:47:22 +0200

    New algorithm to be tested

--- drop_test/main.py
+++ drop_test/main.py
@@ -1,5 +1,11 @@
 from pathlib import Path
 
+def calculate_last_part(part_count, height):
+    base = int(height / part_count)
+    if (part_count % 2 == 0):
+        return int(base - part_count + 1)
+    return int(base - (part_count - 1) / 2)
+
 def calculate_min_drops_with_parts(part_count, height):
     part_size = int(height / part_count)
 
@@ -21,15 +27,25 @@ def max_num_of_drops(object_count, height):
             min_drop_count = min(min_drop_count, 
                                  calculate_min_drops_with_parts(part_count, height))
             part_count += 1
+        #print(f"Part size: {height}, min drop {int(min_drop_count)}")
 
-        return int(min_drop_count)
-    return 0
+    else:
+        while (part_count <= min_drop_count):
+            min_drop_count = min(min_drop_count, 
+                                 part_count + max_num_of_drops(object_count - 1, 
+                                                               calculate_last_part(part_count, height)) - 1)
+            part_count += 1
+            # print(f"Total min drop count: {min_drop_count}")
+    return int(min_drop_count)
 
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
     for line in data.splitlines():
         parts = line.split(", ")
         print(max_num_of_drops(int(parts[0]), int(parts[1])))
+
+    # print(max_num_of_drops(3, 100))
+
 
 
 if __name__ == "__main__":
