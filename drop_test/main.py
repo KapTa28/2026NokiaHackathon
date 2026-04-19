c371acf13ec2e1eacf2508e081db8632b903c2af
Kapeller Tamás Imre <diak_105.kapeller.tamas.imre@10.c.2025.com>
2026-04-19 17:22:03 +0200

    New approach

--- drop_test/main.py
+++ drop_test/main.py
@@ -1,16 +1,24 @@
 from pathlib import Path
 from math import ceil
 
-def max_num_of_drops(object_count, height):
-    if (object_count == 1): return height
-
+def drops_with_two_objects(height):
     part_count = 1
     min_drop_count = height
     while (part_count < min_drop_count):
         min_drop_count = min(min_drop_count, 
-                             ceil(part_count * (object_count - 1) / object_count) + max_num_of_drops(object_count - 1, 
-                                                                     ceil(height / part_count) - 1))
+                             ceil(part_count / 2) + int(height / part_count))
         part_count += 1
+    return min_drop_count
+    
+def max_num_of_drops(object_count, height):
+    if (object_count == 1): return height
+
+    min_drop_count = drops_with_two_objects(height)
+    height = min_drop_count
+    for i in range(2, object_count):
+        min_drop_count -= drops_with_two_objects(height)
+        height = min_drop_count
+
     return int(min_drop_count)
 
 def main():
@@ -19,6 +27,8 @@ def main():
         parts = line.split(", ")
         print(max_num_of_drops(int(parts[0]), 
                                int(parts[1])))
+    # print(max_num_of_drops(2, 30))
 
 if __name__ == "__main__":
     main()
+
