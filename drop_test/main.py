c6d2e95d779a2dd236f2065907f358d19b548911
Kapeller Tamás Imre <diak_105.kapeller.tamas.imre@10.c.2025.com>
2026-04-19 16:09:29 +0200

    New recursive algorithm with seemingly correct results

--- drop_test/main.py
+++ drop_test/main.py
@@ -1,52 +1,24 @@
 from pathlib import Path
-
-def calculate_last_part(part_count, height):
-    base = int(height / part_count)
-    if (part_count % 2 == 0):
-        return int(base - part_count + 1)
-    return int(base - (part_count - 1) / 2)
-
-def calculate_min_drops_with_parts(part_count, height):
-    part_size = int(height / part_count)
-
-    if (part_size < height - part_count * part_size):
-        return height
-
-    if (part_count % 2 == 0):
-        return part_size + part_count - 1
-
-    return part_size + (part_count - 1) / 2
+from math import ceil
 
 def max_num_of_drops(object_count, height):
     if (object_count == 1): return height
 
     part_count = 1
     min_drop_count = height
-    if (object_count == 2):
-        while (part_count <= min_drop_count):
-            min_drop_count = min(min_drop_count, 
-                                 calculate_min_drops_with_parts(part_count, height))
-            part_count += 1
-        #print(f"Part size: {height}, min drop {int(min_drop_count)}")
-
-    else:
-        while (part_count <= min_drop_count):
-            min_drop_count = min(min_drop_count, 
-                                 part_count + max_num_of_drops(object_count - 1, 
-                                                               calculate_last_part(part_count, height)) - 1)
-            part_count += 1
-            # print(f"Total min drop count: {min_drop_count}")
+    while (part_count < min_drop_count):
+        min_drop_count = min(min_drop_count, 
+                             ceil(part_count / 2) + max_num_of_drops(object_count - 1, 
+                                                                     int(height / part_count)))
+        part_count += 1
     return int(min_drop_count)
 
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
     for line in data.splitlines():
         parts = line.split(", ")
-        print(max_num_of_drops(int(parts[0]), int(parts[1])))
-
-    # print(max_num_of_drops(3, 100))
-
-
+        print(max_num_of_drops(int(parts[0]), 
+                               int(parts[1])))
 
 if __name__ == "__main__":
     main()
